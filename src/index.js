dbca993d1fc2523dbd0465a99b34524bf3dc4114
Viren Ratan <viren@viren.com.au>
2019-03-18 14:43:13 +1100

    🎨 restructured project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+// @flow
+import 'react-app-polyfill/ie9'
+import 'sanitize.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+import App from './components/App'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
